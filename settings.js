2bd547edfd327ce1add033081aad8e179ab90ed2
Onuzulike Anthony Ifechukwu <onuzulikeanthony432@gmail.com>
2024-12-13 15:19:00 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const chalk = require("chalk")
 const fs = require("fs")
 //auto presence update
-global.autoTyping = false //auto tying in gc (true to on, false to off)
-global.autoRecord = false //auto recording (true to on, false to off)
+global.autoTyping = true //auto tying in gc (true to on, false to off)
+global.autoRecord = true //auto recording (true to on, false to off)
 global.autoblockmorroco = true //auto block 212 (true to on, false to off)
 global.autokickmorroco = true //auto kick 212 (true to on, false to off) 
-global.antispam = false//auto kick spammer (true to on, false to off)
+global.antispam = true//auto kick spammer (true to on, false to off)
 //===============SETTING MENU==================\\
 var _0x6e9f5f = _0x440c;
 function _0x440c(_0xff7e8a, _0x55b825) {
@@ -46,7 +46,7 @@ function _0xe1c6(_0x488713, _0x4b148) {
   }, _0xe1c6(_0x488713, _0x4b148);
 }
 function _0x8e09() {
-  var _0x15a2b3 = ["ownerNumber", "2348133729715", "1042636teAGzd", "wagc", "websitex", "botnumber", "Sticker created by", "2348133729715@s.whatsapp.net", "2VoTTbt", "ᴘᴀᴛʀᴏɴ-ᴍᴅ", "packname", "460476jKclmm", "ᴘᴀᴛʀᴏɴ-ᴍᴅ 🚹", "331209drywgW", "https://github.com/Itzpatron/PATRON-MD", "ownernumber", "ᴘᴀᴛʀᴏɴ-ᴍᴅ🚹", "1256664XwfMbY", "10WbDnKq", "author", "456740fytdfb", "38255xwXZfH", "6oiKDdL", "botscript", "botname", "https://whatsapp.com/channel/0029Val0s0rIt5rsIDPCoD2q", "160282IElaUI"];
+  var _0x15a2b3 = ["ownerNumber", "2349153232038", "1042636teAGzd", "wagc", "websitex", "botnumber", "Sticker created by", "2349153232038@s.whatsapp.net", "2VoTTbt", "ᴘᴀᴛʀᴏɴ-ᴍᴅ", "packname", "460476jKclmm", "ᴘᴀᴛʀᴏɴ-ᴍᴅ 🚹", "331209drywgW", "https://github.com/Itzpatron/PATRON-MD", "ownernumber", "ᴘᴀᴛʀᴏɴ-ᴍᴅ🚹", "1256664XwfMbY", "10WbDnKq", "author", "456740fytdfb", "38255xwXZfH", "6oiKDdL", "botscript", "botname", "https://whatsapp.com/channel/0029Val0s0rIt5rsIDPCoD2q", "160282IElaUI"];
   _0x8e09 = function () {
     return _0x15a2b3;
   };
@@ -63,10 +63,10 @@ function _0x8e09() {
     }
   }
 }(_0x8e09, 105820), global[_0x1bcf00(146)] = _0x1bcf00(131), global[_0x1bcf00(137)] = _0x1bcf00(150), global[_0x1bcf00(127)] = "2348133729715", global.ownername = _0x1bcf00(138), global[_0x1bcf00(149)] = [_0x1bcf00(129)], global.ownerweb = "https://whatsapp.com/channel/0029Val0s0rIt5rsIDPCoD2q", global[_0x1bcf00(153)] = _0x1bcf00(147), global[_0x1bcf00(152)] = _0x1bcf00(147), global.saluran = _0x1bcf00(147), global.themeemoji = "🚹", global.wm = "ᴘᴀᴛʀᴏɴ-ᴍᴅ", global[_0x1bcf00(145)] = _0x1bcf00(136), global[_0x1bcf00(132)] = _0x1bcf00(128), global[_0x1bcf00(141)] = _0x1bcf00(134))
-global.creator = "2348133729715@s.whatsapp.net"
-global.bankname = "MONIEPOINT"
-global.banknumber = "5518447058"
-global.bankowner = "FADARE"
+global.creator = "2349153232038@s.whatsapp.net"
+global.bankname = "OPAY"
+global.banknumber = "8127957387"
+global.bankowner = "ONUZULIKE ANTHONY IFECHUKWU"
 //======================== CPANEL COMMAND ===========================\\
 global.domain = '-' // Fill in your domain, don't put a / at the end of the link
 global.apikey = '-' // Fill Apikey
